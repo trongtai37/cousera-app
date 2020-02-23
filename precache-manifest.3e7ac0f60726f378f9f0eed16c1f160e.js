e73f4f44460b19844b2f1f117dc5b33ec701ea0d
trongtai37 <42902235+trongtai37@users.noreply.github.com>
2020-02-23 09:05:00 +0000

    Deploying to gh-pages from master - 297b1073255b444714b70a705ce3eb5aecd12aac 🚀

---
+++ precache-manifest.3e7ac0f60726f378f9f0eed16c1f160e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af56a27fecb200408d9e85fa93cf426b",
+    "url": "/trongtai37/confusion-app/index.html"
+  },
+  {
+    "revision": "2f6d369107ec05780e82",
+    "url": "/trongtai37/confusion-app/static/css/2.68b81609.chunk.css"
+  },
+  {
+    "revision": "0351fcb5f8eb5658a60a",
+    "url": "/trongtai37/confusion-app/static/css/main.e968717d.chunk.css"
+  },
+  {
+    "revision": "2f6d369107ec05780e82",
+    "url": "/trongtai37/confusion-app/static/js/2.c28d590e.chunk.js"
+  },
+  {
+    "revision": "0351fcb5f8eb5658a60a",
+    "url": "/trongtai37/confusion-app/static/js/main.3f7ef6b2.chunk.js"
+  },
+  {
+    "revision": "e47d98576433edaa704b",
+    "url": "/trongtai37/confusion-app/static/js/runtime~main.e8673049.js"
+  },
+  {
+    "revision": "96d6bd20976fae9cf5fdaccec2948966",
+    "url": "/trongtai37/confusion-app/static/media/buffet.96d6bd20.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/trongtai37/confusion-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/trongtai37/confusion-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/trongtai37/confusion-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/trongtai37/confusion-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/trongtai37/confusion-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
